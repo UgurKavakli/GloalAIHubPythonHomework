cd8aa46e8375f7cfbfc25ec8970f49852882eed2
UgurKavakli <60283891+UgurKavakli@users.noreply.github.com>
2020-12-24 23:09:28 +0300

    First Upload

---
+++ HW2.py
@@ -0,0 +1,59 @@
+my_list = []
+def ErrorLine():
+    print("\nType Error: Please enter a valid value.")
+
+
+while True:
+    name = input("Please enter your first name: ")
+    if (name.isalpha()):
+        my_list.append(name)
+        break
+    else:
+        ErrorLine()
+        continue
+
+while True:
+    surname = input("Please enter your last name: ")
+    if (surname.isalpha()):
+        my_list.append(surname)
+        break
+    else:
+        ErrorLine()
+        continue
+
+while True:
+    try:
+        age = int(input("Please enter your age: "))
+    except ValueError:
+        ErrorLine()
+        continue
+    if (age<0):
+        ErrorLine()
+        continue
+    else:
+        my_list.append(age)
+        break
+    
+while True:
+    try:
+        year = int(input("Please enter the year of your birth: "))
+    except ValueError:
+        ErrorLine()
+        continue
+    if (year<0):
+        ErrorLine()
+        continue
+    else:
+        my_list.append(year)
+        break
+    
+print("\n")
+
+for i in my_list:
+    print(i)
+
+print("\n")
+if (age<18):
+    print("You can't go out because it's too dangerous.")
+else:
+    print("You can go out to the street.")
